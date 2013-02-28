33453b3bca30d2b7eeba954ab4caf03e0b1f4430
Rachel Conley <rachelconley265@gmail.com>
2013-02-28 09:35:51 -0600

    My first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fettc::Application.config.secret_token = 'f3223e35bf075b9b7e5cf643320dea852b5a090dc55c1e2a797fd0b2d940409b2de2cca3d6e02dc3234d37cc6a89229a2a15fab7552ff6d570898650144d9d14'
